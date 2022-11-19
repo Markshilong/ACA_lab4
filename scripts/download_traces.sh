edaf91b163474d945f562158c23116a63e3b8761
Markshilong <shilonghappy@gmail.com>
2022-11-19 18:12:10 -0500

    ghb

--- scripts/download_traces.sh
+++ scripts/download_traces.sh
@@ -4,5 +4,5 @@ mkdir -p $PWD/../traces
 i=0
 while read LINE
 do
-    wget -P $PWD/../traces -c http://hpca23.cse.tamu.edu/champsim-traces/speccpu/$LINE
+    wget -P $PWD/../traces -c https://dpc3.compas.cs.stonybrook.edu/champsim-traces/speccpu/$LINE
 done < lab_traces.txt
